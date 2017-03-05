496d0f476bbc7eef66e62763ad2ce8212a7b1750
cool168 <cool168@users.noreply.github.com>
2017-03-05 20:51:35 +0800

    Create ss-server.sh

---
+++ ss-server.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+
+
+echo ${SS_UDP="-u"}
+
+echo ${SS_OTA=""}
+
+sleep 1
+
+if [ $ENABLE_CFGFILE = "yes" ]
+then
+	ss-server -u -c /etc/ss-config.json
+else
+	ss-server -s 0.0.0.0 -p $SS_SERVER_PORT -m $SS_SERVER_METHOD -k $SS_SERVER_PWD -t 60 $SS_UDP $SS_OTA
+fi
