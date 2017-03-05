527503fc50f1a13b468c76562935e68d3fed8e50
cool168 <cool168@users.noreply.github.com>
2017-03-05 21:11:37 +0800

    Update ss-local.sh

--- ss-local.sh
+++ ss-local.sh
@@ -4,7 +4,7 @@ echo ${SS_LOCAL_PORT=1080}
 
 echo ${SS_SERVER_PORT=8989}
 
-echo ${SS_SERVER_PWD='aes-256-cfb'}
+echo ${SS_SERVER_PWD='njkhmuDKxMwjGZw3stLDNQii'}
 
 echo ${SS_SERVER_METHOD='aes-256-cfb'}
 
